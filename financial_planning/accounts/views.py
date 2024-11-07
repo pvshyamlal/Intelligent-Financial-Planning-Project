0a47f9a777cbc1db5578b10dabe4ebda171137c0
pvshyamlal <121094165+pvshyamlal@users.noreply.github.com>
2024-11-07 15:58:29 +0530

    codes pushed

---
+++ financial_planning/accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import UserRegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after registration
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a home page or dashboard
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
