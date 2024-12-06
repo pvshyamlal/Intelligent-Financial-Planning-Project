aa3469bbbfc14318447365853c70d17638eedc5a
pvshyamlal <121094165+pvshyamlal@users.noreply.github.com>
2024-12-06 23:13:21 +0530

    minute changes added noifications.html

--- financial_planner/accounts/urls.py
+++ financial_planner/accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('home/', views.home, name='home'),  # Homepage
     path('profile/', views.profile, name='profile'),
+    path('notification/', views.notification, name='notification'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('change-password/', views.change_password, name='change_password'), 
--- financial_planner/accounts/views.py
+++ financial_planner/accounts/views.py
@@ -30,6 +30,9 @@ def profile(request):
         messages.success(request, f"Welcome back, {request.user.username}!")
     return render(request, 'accounts/profile.html')
 
+def notification(request):
+    return render(request, 'accounts/notification.html')
+
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
@@ -216,6 +219,7 @@ def set_budget(request):
     return JsonResponse({'success': False, 'message': 'Invalid request method.'}, status=405)
 
 @login_required
+
 def add_expenses(request):
     if request.method == 'POST':
         form = ExpenseForm(request.POST)
